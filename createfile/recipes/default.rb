227bef2ac5aa76c7d9c2af90c849043f2aa168c2
megahednour <35898404+megahednour@users.noreply.github.com>
2019-03-07 13:29:31 -0500

    Update default.rb

--- createfile/recipes/default.rb
+++ createfile/recipes/default.rb
@@ -27,7 +27,7 @@ end
 template "/home/ubuntu/data2.json" do
   source "data2.json.erb"
   mode 0644
-  variables (
+  variables(
       :a_boolean => true,
       :a_dob => "03_05_1983",
       :a_string => "string"
